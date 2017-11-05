ed1348376e2404ba491e4f15f5905812da1b21b9
Mikart <mikart.dev@gmail.com>
2017-11-05 22:51:44 +0100

    Initial commit

---
+++ java-api/src/main/java/pl/bobrzadywizja/dogapi/DogapiApplication.java
@@ -0,0 +1,12 @@
+package pl.bobrzadywizja.dogapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogapiApplication.class, args);
+	}
+}
